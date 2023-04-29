0e4f2dace0a7004e92089b050054fc08bebc9ed9
uncledata <tomas1991@gmail.com>
2023-04-29 22:40:03 +0300

    dagster ingest

--- dagster/dagster-demo/dagster_demo/assets/__init__.py
+++
@@ -1,7 +0,0 @@
-import pickle
-import pandas as pd
-from datetime import timedelta
-from dagster import asset
-
-
-
--- dagster/dagster-demo/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd /opt/dagster/app
-pip install -e '.[dev]'
-
-dagit -h 0.0.0.0 -p 3000 -w ./workspace.yaml
